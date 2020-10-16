05c5c8d62abdaf8e24b9d807b3c787b90d7a3083
Sacha FROMENT <sacha@hjkl.it>
2020-10-16 15:01:06 +0200

    initial commit

---
+++ templates/tweetduck.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$PackageName  = $env:ChocolateyPackageName
+$App = $PackageName.Split(".")[0]
+$InstallDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$PackageArgs = @{
+  PackageName   = $PackageName
+  UnzipLocation = $InstallDir
+  FileType      = 'exe'
+  Url           = "$url"
+
+  SoftwareName  = 'tweetduck*'
+
+  Checksum      = "$checksum"
+  ChecksumType  = 'sha512'
+  
+  SilentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  ValidExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @PackageArgs
+$InstallLocation = Get-AppInstallLocation "$PackageName*"
+
+Write-Host "$PackageName registered as $App"
